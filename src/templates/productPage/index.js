389a4fc2c054eaa71ca088e85559fa07293a23f4
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-01-28 15:08:34 +0100

    refactor Layout into own folder

--- src/templates/productPage/index.js
+++ src/templates/productPage/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../../components/layout"
+import Layout from "../../components/Layout"
 
 export default ({ data }) => {
   const product = data.shopifyProduct
