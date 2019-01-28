bc11ff66084a8ebef7b0c3793cbfd1918f8a2ddf
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-01-28 17:50:25 +0100

    layout import changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import SEO from '../components/seo'
 import ProductGrid from '../components/ProductGrid'
 
--- src/templates/productPage/index.js
+++ src/templates/productPage/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Layout from "../../components/Layout"
+import Layout from "../../components/layout"
 
 export default ({ data }) => {
   const product = data.shopifyProduct
