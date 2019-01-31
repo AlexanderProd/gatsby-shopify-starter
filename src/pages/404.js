b39b9e66c3517bf195337b581efbb951e1ab1f60
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-01-31 09:48:49 +0100

    removed layout component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import Layout from '../layouts'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../layouts'
 import SEO from '../components/seo'
 import ProductGrid from '../components/ProductGrid'
 
