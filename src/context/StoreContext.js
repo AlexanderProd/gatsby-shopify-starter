2fd1d1b9336e9c5ff8d75b568b0f6886d7f0657c
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-01-31 19:21:44 +0100

    removed comments

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -6,45 +6,6 @@ const client = Client.buildClient({
   storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38'
 });
 
-/* const defaultContextValue = {
-  client,
-  data: {
-    // set your initial data shape here
-    menuOpen: false,
-  },
-  test: '12345',
-  set: () => { },
-}
-
-const { Provider, Consumer } = React.createContext(defaultContextValue)
-
-class ContextProviderComponent extends React.Component {
-  constructor() {
-    super()
-
-    this.setData = this.setData.bind(this)
-    this.state = {
-      ...defaultContextValue,
-      set: this.setData,
-    }
-  }
-
-  setData(newData) {
-    this.setState(state => ({
-      data: {
-        ...state.data,
-        ...newData,
-      },
-    }))
-  }
-
-  render() {
-    return <Provider value={this.state}>{this.props.children}</Provider>
-  }
-}
-
-export { Consumer as default, ContextProviderComponent } */
-
 export const defaultStoreContext = {
   client,
   isCartOpen: false,
