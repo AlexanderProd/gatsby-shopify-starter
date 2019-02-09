f6253c41f262067ed2dba46a3b8f7ce085ed8b8e
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-02-09 20:05:32 +0100

    switched ProductForm to function component

--- src/components/ProductForm/index.js
+++ src/components/ProductForm/index.js
@@ -1,115 +1,77 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import PropTypes from 'prop-types'
 
 import StoreContext from '../../context/StoreContext'
 import VariantSelector from './VariantSelector'
 
-class ProductForm extends React.Component {
-  state = {
-    variant:
-      this.props.product.variants.length === 1
-        ? this.props.product.variants[0].shopifyId
-        : '',
-    quantity: 1,
-    errors: [],
+const ProductForm = props => {
+  const [quantity, setQuantity] = useState(1)
+  const [variant, setVariant] = useState(props.product.variants[0])
+  const context = useContext(StoreContext)
+
+  const { variants } = props.product
+  const hasVariants = variants.length > 1
+  const isOutOfStock = !hasVariants && !variants[0].availableForSale
+  // const variant = /* this.state.selectedVariant || */ variants[0]
+  // const variantQuantity = this.state.quantity || 1
+  
+  const handleQuantityChange = event => {
+    setQuantity(event.target.value)
+  }
+  
+  const handleOptionChange = event => {
+    const { target } = event
+    setVariant({
+      ...variant,
+      [target.name]: target.value,
+    })
+    console.log(variant)
+  }
+  
+  const handleSubmit = () => {
+    console.log(context.client.product.helpers.variantForOptions(props.product, {
+      Color: "Brown",
+      Size: "7",
+    }))
   }
 
-  handleChange = event => {
-    event.preventDefault()
-
-    if (event.target.value) {
-      const errors = this.state.errors
-
-      const errorIdx = errors.findIndex(
-        error => error.field === event.target.name
-      )
-
-      errors.splice(errorIdx, 1)
-
-      if (~errorIdx) {
-        this.setState({ errors: errors })
-      }
-    }
-
-    console.log(event.target.name, event.target.value)
-    this.setState({ [event.target.name]: event.target.value })
-  }
-
-  handleSubmit = callback => event => {
-    event.preventDefault()
-
-    const errors = []
-
-    if (this.state.quantity < 1) {
-      errors.push({
-        field: 'quantity',
-        msg: 'Choose a <b>quantity</b> of 1 or more.',
+  const variantSelectors = hasVariants
+    ? props.product.options.map(option => {
+        return (
+          <VariantSelector
+            onChange={handleOptionChange}
+            key={option.id.toString()}
+            option={option}
+          />
+        )
       })
-    }
-
-    if (this.state.variant === '' || this.state.variant === '.') {
-      errors.push({
-        field: 'variant',
-        msg: 'Please select a <b>size</b>.',
-      })
-    }
-
-    if (errors.length) {
-      this.setState({ errors: errors })
-      return
-    }
-
-    callback(this.state.variant, this.state.quantity)
-  }
-
-  render() {
-    const { variants } = this.props.product
-    const hasVariants = variants.length > 1
-    const isOutOfStock = !hasVariants && !variants[0].availableForSale
-    const variant = /* this.state.selectedVariant || */ variants[0]
-    // const variantQuantity = this.state.quantity || 1
-
-    const variantSelectors = hasVariants
-      ? this.props.product.options.map(option => {
-          return (
-            <VariantSelector
-              onChange={this.handleChange}
-              key={option.id.toString()}
-              option={option}
-            />
-          )
-        })
-      : null
-
-    return (
-      <StoreContext.Consumer>
-        {({ addVariantToCart }) => (
-          <form onSubmit={this.handleSubmit(addVariantToCart)} noValidate>
-            <span className="Product__price">${variant.price}</span>
-            {variantSelectors}
-            <label htmlFor="quantity">Qty.</label>
-            <input
-              type="number"
-              id="quantity"
-              name="quantity"
-              min="1"
-              step="1"
-              onChange={this.handleChange}
-              value={this.state.quantity}
-            />
-            <button
-              type="submit"
-              className="Product__buy button"
-              disabled={isOutOfStock}
-            >
-              Add to Cart
-            </button>
-            {isOutOfStock && <p>This Product is out of Stock!</p>}
-          </form>
-        )}
-      </StoreContext.Consumer>
-    )
-  }
+    : null
+  
+  return (
+    <>
+      <span className="Product__price">${variant.price}</span>
+      {variantSelectors}
+      <label htmlFor="quantity">Qty.</label>
+      <input
+        type="number"
+        id="quantity"
+        name="quantity"
+        min="1"
+        step="1"
+        onChange={handleQuantityChange}
+        value={quantity}
+      />
+      <button
+        type="submit"
+        className="Product__buy button"
+        disabled={isOutOfStock}
+        onClick={handleSubmit}
+      >
+        Add to Cart
+      </button>
+      {isOutOfStock && <p>This Product is out of Stock!</p>}
+    </>
+  )
 }
 
 ProductForm.propTypes = {
@@ -139,7 +101,11 @@ ProductForm.propTypes = {
         price: PropTypes.string,
         title: PropTypes.string,
         shopifyId: PropTypes.string,
-      })
+        selectedOptions: PropTypes.shape({
+          name: PropTypes.string,
+          value: PropTypes.string,
+        })
+      }),
     ),
   }),
   addVariantToCart: PropTypes.func,
