2004916736ddb7fd0eef095e60adc10b6bc54f05
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-02-12 22:07:43 +0100

    renamed acces token

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
         // to select "Allow this app to access your storefront data using the
         // Storefront API".
         // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
-        accessToken: process.env.ACCESSTOKEN,
+        accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
 
         // Set verbose to true to display a verbose output on `npm run develop`
         // or `npm run build`. This prints which nodes are being fetched and how
