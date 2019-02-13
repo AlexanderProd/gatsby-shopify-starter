3a78d8cabad1ee6cd701431b60a76b2294cf2373
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-02-13 13:49:41 +0100

    created cart component

---
+++ src/components/Cart/index.js
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react'
+
+import StoreContext from '../../context/StoreContext'
+import LineItem from './LineItem'
+
+const Cart = () => {
+  const context = useContext(StoreContext)
+  const { checkout } = context
+
+  const handleCheckout = () => {
+    window.open(checkout.webUrl)
+  }
+   
+  const line_items = checkout.lineItems.map(line_item => {
+    return (
+      <LineItem
+        key={line_item.id.toString()}
+        line_item={line_item}
+      />
+    )
+  })
+
+  return (
+    <div>
+      {<ul>
+        {line_items}
+      </ul>}
+      <h2>Subtotal</h2>
+      <p>$ {checkout.subtotalPrice}</p>
+      <br/>
+      <h2>Taxes</h2>
+      <p>$ {checkout.totalTax}</p>
+      <br/>
+      <h2>Total</h2>
+      <p>$ {checkout.totalPrice}</p>
+      <br/>
+      <button
+        onClick={handleCheckout}
+      >
+        Check out
+      </button>
+    </div>
+  )
+}
+
+export default Cart
