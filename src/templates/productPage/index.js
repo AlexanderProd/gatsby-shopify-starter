c9e85bdf9760bb0034e3f03251669c209d590d30
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-02-16 15:06:59 +0100

    added rebass grid

--- src/templates/productPage/index.js
+++ src/templates/productPage/index.js
@@ -1,39 +1,24 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { Flex, Box } from '@rebass/grid/emotion'
 
 import ProductForm from '../../components/ProductForm'
 
 const ProductPage = ({ data }) => {
   const product = data.shopifyProduct
   return (
-    <>
-      <div
-        style={{
-          display: `grid`,
-          'grid-template-columns': `repeat(12, 1fr)`,
-          'grid-template-rows': `100% 100% 100%`,
-        }}
-      >
-        <div
-          style={{
-            'grid-column': `span 6`,
-          }}
-        >
-          {product.images.map(x => (
-            <img src={x.originalSrc} key={x.id} alt={product.title} />
-          ))}
-        </div>
-        <div
-          style={{
-            'grid-column': `span 6`,
-          }}
-        >
-          <h1>{product.title}</h1>
-          <p dangerouslySetInnerHTML={{ __html: product.descriptionHtml }} />
-          <ProductForm product={product} />
-        </div>
-      </div>
-    </>
+    <Flex flexWrap='wrap'>
+      <Box pr={[null, 3]} width={[1, 1/2]}>
+        {product.images.map(x => (
+          <img src={x.originalSrc} key={x.id} alt={product.title} />
+        ))}
+      </Box>
+      <Box width={[1, 1/2]}>
+        <h1>{product.title}</h1>
+        <p dangerouslySetInnerHTML={{ __html: product.descriptionHtml }} />
+        <ProductForm product={product} />
+      </Box>
+    </Flex >
   )
 }
 
