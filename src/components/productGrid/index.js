d091efc66c79dc378f9e10ed0bd87b65bf319af5
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-02-22 12:35:37 +0100

    switched to staticQuery hook

--- src/components/productGrid/index.js
+++ src/components/productGrid/index.js
@@ -1,64 +1,65 @@
 import React from 'react'
-import { graphql, StaticQuery, Link } from 'gatsby'
+import { useStaticQuery,  graphql, Link } from 'gatsby'
 import { Flex, Box } from '@rebass/grid/emotion'
 
 import { Img } from '../../utils/styles'
 
-const ProductGrid = () => (
-  <Flex flexWrap='wrap' mx={-2}>
-    <StaticQuery
-      query={graphql`
-        {
-          allShopifyProduct(
-            sort: {
-              fields: [createdAt]
-              order: DESC
-            }
-          ) {
-            edges {
-              node {
+const ProductGrid = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allShopifyProduct(
+          sort: {
+            fields: [createdAt]
+            order: DESC
+          }
+        ) {
+          edges {
+            node {
+              id
+              title
+              handle
+              createdAt
+              images {
                 id
-                title
-                handle
-                createdAt
-                images {
-                  id
-                  originalSrc
-                  localFile {
-                    childImageSharp {
-                      fluid(maxWidth: 910) {
-                        ...GatsbyImageSharpFluid_withWebp_tracedSVG
-                      }
+                originalSrc
+                localFile {
+                  childImageSharp {
+                    fluid(maxWidth: 910) {
+                      ...GatsbyImageSharpFluid_withWebp_tracedSVG
                     }
                   }
                 }
-                variants {
-                  price
-                }
+              }
+              variants {
+                price
               }
             }
           }
         }
-      `}
-      render={data =>
-        data.allShopifyProduct.edges.map(x => (
-          <Box
-          width={[1, 1 / 2, 1 / 3]}
-          px={2}
-          key={x.node.id}
-          >
-            <Link to={`/product/${x.node.handle}/`}>
-              <Img
-                fluid={x.node.images[0].localFile.childImageSharp.fluid}
-                alt={x.node.handle}
-              />
-            </Link>
-            <p>{x.node.title}</p>
-          </Box>
-        ))
       }
-    />
-  </Flex>
-)
+    `
+  )
+
+  return (
+    <Flex flexWrap='wrap' mx={-2}>
+      {data.allShopifyProduct.edges.map(x => (
+        <Box
+        width={[1, 1 / 2, 1 / 3]}
+        px={2}
+        key={x.node.id}
+        >
+          <Link to={`/product/${x.node.handle}/`}>
+            <Img
+              fluid={x.node.images[0].localFile.childImageSharp.fluid}
+              alt={x.node.handle}
+            />
+          </Link>
+          <p>{x.node.title}</p>
+        </Box>
+      ))}
+    </Flex>
+  )
+}
 
 export default ProductGrid
